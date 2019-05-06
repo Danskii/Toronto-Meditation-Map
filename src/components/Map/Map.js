8a7a912dbcc1163d46804bb144b331b0b6c07b29
Dan Puiatti <dpuiatti@gmail.com>
2019-05-06 18:45:14 -0400

    added items

---
+++ src/components/Map/Map.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { compose, withProps } from "recompose"
+const {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  KmlLayer,
+} = require("react-google-maps");
+const Map = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyC80_y_wRg1tKAM59tSszeIiY6tEahwUmc",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `100vh` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)(props =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat: 43.66249657567853, lng: -79.39037738471677 }}
+
+  >
+    <KmlLayer
+      url={"http://www.google.com/maps/d/kml?mid=1jhR92EDAFYtvkJFLZDO9CiEADRLmqiqb" + "&ver=" + generateRandom()}
+      options={{ preserveViewport: true }}
+    />
+  </GoogleMap>
+);
+function generateRandom() {
+  return Math.random() * 10000000000000000
+}
+export default Map
