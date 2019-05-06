d34168cac97f0b9708452f6dfd7e37a7e85928f1
Dan Puiatti <dpuiatti@gmail.com>
2019-05-06 19:25:29 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './components/Map'
+import Map from './components/Map';
+import Footer from './components/Footer';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-      ay
       <Map/>
+      <Footer/>
       </div>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="Footer">
+      
+<Container>
+
+  <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+
+      </div>
+    );
+  }
+}
+export default Footer;
