cd01a29c61670b93e4f4c210750c746387a8780b
Daniel Puiatti <puiattid@corp01.oct.on.ca>
2019-05-09 12:10:05 -0400

    changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,20 +1,25 @@
-import React, { Component } from 'react';
-import {Container, Row, Col} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Main extends Component {
   render() {
     return (
       <div className="Main">
-      
-<Container>
+        <div className="ripple-background">
+          <div className="circle xxlarge shade1" />
+          <div className="circle xlarge shade2" />
+          <div className="circle large shade3" />
+          <div className="circle mediun shade4" />
+          <div className="circle small shade5" />
+        </div>
 
-  <Row>
-    <Col>1 of 3</Col>
-    <Col>2 of 3</Col>
-    <Col>3 of 3</Col>
-  </Row>
-</Container>
-
+        <Container>
+          <Row>
+            <Col>1 of 3</Col>
+            <Col>2 of 3</Col>
+            <Col>3 of 3</Col>
+          </Row>
+        </Container>
       </div>
     );
   }
